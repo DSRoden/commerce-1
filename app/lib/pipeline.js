120c6ac11009ff8986efe867ae1265b808268fe9
DSRoden <Daniel.S.Roden@gmail.com>
2014-08-04 21:19:47 -0500

    commerce website

--- app/lib/pipeline.js
+++ app/lib/pipeline.js
@@ -2,6 +2,7 @@
 
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var methodOverride = require('express-method-override');
 var home = require('../controllers/home');
 var items = require('../controllers/items');
 
@@ -9,6 +10,7 @@ module.exports = function(app, express){
   app.use(morgan('dev'));
   app.use(express.static(__dirname + '/../static'));
   app.use(bodyParser.urlencoded({extended:true}));
+  app.use(methodOverride());
 
   app.get('/', home.index);
   app.get('/about', home.about);
@@ -16,6 +18,9 @@ module.exports = function(app, express){
 
   app.get('/items/new', items.init);
   app.post('/items', items.create);
+  app.get('/items', items.index);
+  app.get('/items/:id', items.show);
+  app.delete('/items/:id/', items.destroy);
 
   console.log('Pipeline Configured');
 };
